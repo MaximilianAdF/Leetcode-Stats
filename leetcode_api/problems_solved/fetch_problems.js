e263bcabebb0145ba69dbf46f16ba3a061ed32f0
Maximilian Alvim de Faria <63980031+MaximilianAdF@users.noreply.github.com>
2024-04-06 16:11:53 +0200

    vercel test

--- leetcode_api/problems_solved/fetch_problems.js
+++ leetcode_api/problems_solved/fetch_problems.js
@@ -1,28 +1,48 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var fetch_1 = require("../fetch");
-//Define the graphql query
-var problemsSolvedQuery = "\n    query userProblemsSolved($username: String!) {\n        allQuestionsCount {\n            difficulty\n            count\n        }\n        matchedUser(username: $username) {\n            problemsSolvedBeatsStats {\n                difficulty\n                percentage\n            }\n            submitStatsGlobal {\n                acSubmissionNum {\n                    difficulty\n                    count\n                }\n            }\n        }\n    }\n    ";
-var problemsSolvedData = null;
-//Make the request to GraphQL
-(0, fetch_1.default)({
-    operationName: 'userProblemsSolved',
-    query: problemsSolvedQuery,
-    variables: {
-        username: 'Makimi'
+const { default: graphqlRequest } = require("../fetch.js");
+
+const problemsSolvedQuery = `
+  query userProblemsSolved($username: String!) {
+    allQuestionsCount {
+      difficulty
+      count
     }
-})
-    .then(function (data) {
-    var allQuestionsCount = data.data.allQuestionsCount;
-    var problemsSolvedBeatsStats = data.data.matchedUser.problemsSolvedBeatsStats;
-    var submitStatsGlobal = data.data.matchedUser.submitStatsGlobal.acSubmissionNum;
-    problemsSolvedData = {
-        allQuestionsCount: allQuestionsCount,
-        problemsSolvedBeatsStats: problemsSolvedBeatsStats,
-        submitStatsGlobal: submitStatsGlobal
-    };
-})
-    .catch(function (error) {
-    console.log(error);
-});
-exports.default = problemsSolvedData;
+    matchedUser(username: $username) {
+      problemsSolvedBeatsStats {
+        difficulty
+        percentage
+      }
+      submitStatsGlobal {
+        acSubmissionNum {
+          difficulty
+          count
+        }
+      }
+    }
+  }
+`;
+
+async function fetchProblemsData(username) {
+    try {
+        const request = {
+            operationName: "userProblemsSolved",
+            variables: { username },
+            query: problemsSolvedQuery,
+        };
+
+        const data = await graphqlRequest(request);
+        const allQuestionsCount = data.data.allQuestionsCount;
+        const problemsSolvedBeatsStats = data.data.matchedUser.problemsSolvedBeatsStats;
+        const submitStatsGlobal = data.data.matchedUser.submitStatsGlobal.acSubmissionNum
+
+        return {
+            allQuestionsCount,
+            problemsSolvedBeatsStats,
+            submitStatsGlobal,
+        };
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+}
+
+module.exports = fetchProblemsData;
